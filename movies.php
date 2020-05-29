0e2c0ae5a86236c8a9c52e790eb05ccfd8f44ebc
therainmaiker <57842299+therainmaiker@users.noreply.github.com>
2020-05-29 01:41:36 +0000

    adding html structure

--- movies.php
+++ movies.php
@@ -1,8 +1,22 @@
 <?php
 
-$moviename = $_GET["moviename"];
-
-
-print $moviename;
+$MovieName = $_GET["MovieName"];
+$ActorName = $_GET["ActorName"];
+$MovieDate = $_GET["MovieDate"];
 
 ?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
