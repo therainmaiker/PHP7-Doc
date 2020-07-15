3501bd6fbc37c6d6f72f16b719733d887c0d9094
therainmaiker <57842299+therainmaiker@users.noreply.github.com>
2020-07-15 02:22:24 +0100

    correction of variables

--- tracker.php
+++ tracker.php
@@ -1,9 +1,11 @@
 <?php   
 
-$name = $_GET ['omar'];
-$weightKg = $_GET ['50'];
-$heightCm = $_GET ['155'];
-$heightM = $_GET ['$heightCm/100'];
+$name =  "omar" ;
+$weightKg =  50;
+$heightCm =  155;
+$heightM =  $heightCm/100 ;
+
+
 
 echo $name , $weightKg / $heightCm ;
 ?>
