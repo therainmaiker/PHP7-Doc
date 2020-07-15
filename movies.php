bd42fb044c685fc99b5a23455f572751b52f7742
therainmaiker <57842299+therainmaiker@users.noreply.github.com>
2020-07-15 02:09:52 +0100

    added variables for hight & operators

--- movies.php
+++ movies.php
@@ -23,7 +23,7 @@ $date =$_GET["date"];
     <h1>Information about <?php echo $movie; ?></h1>
 
 <p> Based on the input, here is the information so far: <br>
-<?php echo $actor;?> starred in the movie Avengers which was released in year <?php print $actor; ?>
+<?php echo $actor;?> starred in the movie Avengers which was released in year <?php print $date; ?>
 
 
 
